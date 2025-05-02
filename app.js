af8ce6e4e74d4d3a0a327ad662ff49673defad8e
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2025-05-02 23:53:45 +0530

    send  credentials: true,in cors

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const app = express();
 
 // ceate middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 8080;
 
