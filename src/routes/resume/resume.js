715d780a75820299c20c80fb2b702f4b2d832b85
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2025-05-04 11:56:09 +0530

    bug fix resume api

--- src/routes/resume/resume.js
+++ src/routes/resume/resume.js
@@ -1,6 +1,6 @@
 import express from "express";
 import resume from "../../controller/Resume_Info/resume.js";
-import allResume from "../../controller/Resume_Info/resume.js";
+import allResume from "../../controller/Resume_Info/allResume.js";
 
 const ResumeRouter = express.Router();
 
